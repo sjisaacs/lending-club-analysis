566a2ba18006a4f24cd975bc05dbec0186889725
Steve Isaacs <steve@Steves-MacBook-Pro.local>
2017-01-12 10:33:00 +0100

    Updated

--- Lending Club Analysis.R
+++ Lending Club Analysis.R
@@ -1,3 +1,3 @@
-#The Business Problem would be on Assessing credit worthiness of customers in Lending Club and their probability of defaulting.
+#Business Problem: Assessing credit worthiness of customers in Lending Club and their probability of defaulting.
 
 #Data set: https://www.kaggle.com/wendykan/lending-club-loan-data
